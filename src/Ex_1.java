5191beb97e54346802e7b5174fb02fc89698b8db
Wellington F <wellingtonfrancelino25@gmail.com>
2022-01-26 18:08:25 -0300

    Adicionado Enunciado do Ex1 e Ex2

--- src/Ex_1.java
+++ src/Ex_1.java
@@ -3,8 +3,9 @@ public class Ex_1
 {
     public static void main (String[] args){
 
-        short numero = 100;
+        //1) Mostrar somente os números pares em uma Área de Texto entre 0 e 100
 
+        short numero = 100;
 
         for (short i = 0;i <= 100; i++){
             if (i % 2 == 0){
--- src/Ex_2.java
+++ src/Ex_2.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class Ex_2 {
     public static void main (String[] args){
+
+        /*2) Faça com que o usuário digite um valor em um campo de texto e dentro de uma Area
+        de Texto mostre os números de 0 até o número que o usuário digitou. Verifique se o
+        número que ele digitou é 0 se for 0 mande a mensagem valor inválido.*/
+
         Scanner scan = new Scanner(System.in);
 
         short numero = 0, cont = 0;
 
         System.out.println("Digite um número:");
         numero = scan.nextShort();
-
-
+        
         while(cont <= numero){
             if (numero == 0){
                 System.out.println("Numero invalido, tente novamente: ");
