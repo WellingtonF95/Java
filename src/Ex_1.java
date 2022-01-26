56ae4297d16d5096e31f2d495c71c6d3f2a706f3
Wellington F <wellingtonfrancelino25@gmail.com>
2022-01-25 21:05:52 -0300

    Ex1 Finalizado

---
+++ src/Ex_1.java
@@ -0,0 +1,17 @@
+
+public class Ex_1
+{
+    public static void main (String[] args){
+
+        short numero = 100;
+
+
+        for (short i = 0;i <= 100; i++){
+            if (i % 2 == 0){
+               System.out.println(i);
+            }
+        }
+
+
+    }
+}
